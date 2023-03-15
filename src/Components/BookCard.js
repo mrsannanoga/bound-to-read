8b4a9d6f7b075bbcc56c8e86bbaedd84d9e1453c
Aloosli <lizzwizz@tuta.io>
2023-03-15 19:35:49 +0000

    add BookCard component, add search code and api call to Search.js

---
+++ src/Components/BookCard.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+  display: flex;
+  margin: 1rem;
+`;
+
+const Card = styled.div`
+  display: flex;
+  background-color: #fff;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  &:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
+`;
+
+const BookCover = styled.img`
+  width: 128px;
+  height: auto;
+`;
+
+const BookInfo = styled.div`
+  padding: 1rem;
+`;
+
+const BookTitle = styled.h3`
+  margin-bottom: 0.5rem;
+`;
+
+const BookAuthor = styled.p`
+  margin-bottom: 0.5rem;
+`;
+
+const BookReleaseDate = styled.p`
+  margin-bottom: 0.5rem;
+`;
+
+const BookCard = ({ book }) => {
+  return (
+    <CardContainer>
+      <Card>
+        <BookCover src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />
+        <BookInfo>
+          <BookTitle>{book.volumeInfo.title}</BookTitle>
+          <BookAuthor>{book.volumeInfo.authors.join(', ')}</BookAuthor>
+          <BookReleaseDate>{book.volumeInfo.publishedDate}</BookReleaseDate>
+        </BookInfo>
+      </Card>
+    </CardContainer>
+  );
+};
+
+export default BookCard;
--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import BookCard from '../Components/BookCard';
+
+const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SearchForm = styled.form`
+  display: flex;
+  gap: 1rem;
+  margin-bottom: 2rem;
+`;
+
+const SearchInput = styled.input`
+  font-size: 1rem;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+const SearchButton = styled.button`
+  font-size: 1rem;
+  padding: 0.5rem 1rem;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+`;
+
+const Results = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  justify-content: center;
+`;
 
 const Search = () => {
-  return (
-    <h1>Search</h1>
-  )
-}
+  const [books, setBooks] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-export default Search
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const response = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=inauthor:${searchTerm}&maxResults=20&key=${process.env.REACT_APP_GOOGLE_BOOKS_API_KEY}`,
+      {
+        method: 'GET',
+      },
+    );
+    const data = await response.json();
+    const filteredBooks = data.items.filter((item) => item.volumeInfo.language === 'en');
+    setBooks(filteredBooks);
+  };
+
+  return (
+    <SearchContainer>
+      <h1>Search</h1>
+      <SearchForm onSubmit={handleSearch}>
+        <SearchInput
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Author name"
+        />
+        <SearchButton type="submit">Search</SearchButton>
+      </SearchForm>
+      <Results>
+        {books.map((book) => (
+          <BookCard key={book.id} book={book} />
+        ))}
+      </Results>
+    </SearchContainer>
+  );
+};
+
+export default Search;
