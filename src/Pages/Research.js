7c536cc32ccfdae3d5842a35cc466bf6a2df9d60
amiekisiak <117371691+amiekisiak@users.noreply.github.com>
2023-03-16 19:00:06 +0000

    updated function to get books by subject

--- src/Pages/Research.js
+++ src/Pages/Research.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 function Research() {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
+  const [covers, setCovers] = useState([]);
 
   const handleSearch = async () => {
     const url = `https://openlibrary.org/subjects/${encodeURIComponent(query)}.json?details=true`;
@@ -10,8 +11,19 @@ function Research() {
     const data = await response.json();
 
     // Extract the array of works from the response
-    const works = data.works;
+    const works = data.works || [];
     setResults(works);
+
+    // Fetch cover images for each work and store them in the covers state variable
+    const coverIds = works
+      .map(work => work.cover_id)
+      .filter(id => id !== undefined);
+    const coverUrl = 'https://covers.openlibrary.org/b/id/';
+    const coverUrls = coverIds.map(id => `${coverUrl}${id}-M.jpg`);
+    const coverResponses = await Promise.all(coverUrls.map(url => fetch(url)));
+    const coverBlobs = await Promise.all(coverResponses.map(response => response.blob()));
+    const coverUrlsWithBlobs = coverUrls.map((url, i) => ({ url, blob: coverBlobs[i] }));
+    setCovers(coverUrlsWithBlobs);
   };
 
   return (
@@ -20,12 +32,16 @@ function Research() {
       <button onClick={handleSearch}>Search</button>
 
       <ul>
-        {results.map((work, i) => (
-          <li key={i} style={{ fontSize: '18px' }}>
-            <p>{work.title}</p>
-            <p>{work.authors.map(author => author.name).join(', ')}</p>
-          </li>
-        ))}
+        {results.map((work, i) => {
+          const cover = covers[i] || {};
+          return (
+            <li key={i} style={{ fontSize: '18px' }}>
+              {cover.url && <img src={cover.url} alt="" style={{ height: '100px' }} />}
+              <p>{work.title}</p>
+              <p>{work.authors.map(author => author.name).join(',')}</p>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
