e43155a1d924b12ecdc89c468664de9d6d34d578
Aloosli <lizzwizz@tuta.io>
2023-03-17 08:53:23 +0000

    changed footer text to white, added emoji and cheesy text

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
-
 const Footer = styled.div`
+  background-color: rgb(2, 2, 72);
+`;
 
-background-color: rgb(2, 2, 72)
-`
 const Container = styled.div`
-padding: 2rem;
-font-size: 2rem;
-display: flex;
-justify-content: space-around;
-`
+  padding: 2rem;
+  font-size: 2.5rem;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: #fff;
+`;
 
+const Img = styled.img`
+  width: 2rem;
+  margin-right: 0.5rem;
+`;
 
 const PageFooter = () => {
   return (
     <Footer className='text-center text-white'>
       <Container className='p-4'>
         <div className='text-center'>
-        © 2023 Copyright: All rights reserved.
+          &copy; 2023 Made with <Img src={require("../assets/img/heart.svg").default} alt="Heart" /> by Anna, Aleksandra, and Adrian
         </div>
-        </Container>
+      </Container>
     </Footer>
   );
-}
+};
 
 export default PageFooter;
