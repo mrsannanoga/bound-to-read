f787c6ce5b3de1ba9270baa7aac19840b993d3f1
Aloosli <lizzwizz@tuta.io>
2023-03-17 08:29:20 +0000

    made backround image stretch over whole screen

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,21 +1,42 @@
 import React from "react";
+import styled from "styled-components";
 import { Container } from "react-bootstrap";
 import background from "../assets/img/yellow-background.jpg";
 import Quote from "../Components/RandomQuote/Quote";
 
+const BackgroundWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
+`;
+
+const BackgroundImage = styled.div`
+  background-image: url(${background});
+  background-size: cover;
+  background-position: center center;
+  width: 100%;
+  height: 100%;
+`;
+
+const QuoteWrapper = styled(Container)`
+  position: relative;
+  z-index: 1;
+`;
+
 const Home = () => {
   return (
-    <Container style={{ 
-        backgroundImage: `url(${background})`, 
-        backgroundSize: "cover", 
-        backgroundPosition: "top center",
-        
-    }}>
-      <h1>Home</h1>
-      <Quote />
-    </Container>
+    <>
+      <BackgroundWrapper>
+        <BackgroundImage />
+      </BackgroundWrapper>
+      <QuoteWrapper>
+        <Quote />
+      </QuoteWrapper>
+    </>
   );
 };
 
 export default Home;
-
